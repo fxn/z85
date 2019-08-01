e5cf76ab230321a3240f01a47e4b57ebcaf63411
Xavier Noria <fxn@hashref.com>
2019-08-01 08:43:02 +0200

    relaxes condition
    It is enough to check that the counter is not zero.

--- lib/z85.rb
+++ lib/z85.rb
@@ -7,7 +7,7 @@ module Z85
   def self.encode_with_padding(string)
     counter = 4 - (string.bytesize % 4)
     counter = 0 if counter == 4
-    string += "\0" * counter if counter > 0
+    string += "\0" * counter if counter != 0
     encode(string) + counter.to_s
   end
 end
