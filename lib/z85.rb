a30b48cea9ddece4bdcc4bfb42fb27124d7e0f11
Xavier Noria <fxn@hashref.com>
2019-08-02 20:34:51 +0200

    style

--- lib/z85.rb
+++ lib/z85.rb
@@ -18,23 +18,27 @@ module Z85
       if string.bytesize % 4 != 0
         err "Input length should be 0 mod 4. Please, check Z85.encode_with_padding."
       end
+
       _encode(string)
     end
 
     def encode_with_padding(string)
-      counter = 4 - (string.bytesize % 4)
-      counter == 4 ? _encode(string) + "0" : _encode(string + PADDINGS[counter]) + counter.to_s
+      n  = 4 - (string.bytesize % 4)
+      n == 4 ? _encode(string) + "0" : _encode(string + PADDINGS[n]) + n.to_s
     end
 
     def decode(string)
       if string.bytesize % 5 != 0
         err "Input length should be 0 mod 5. Please, check Z85.decode_with_padding."
       end
+
       _decode(string)
     end
 
     def decode_with_padding(string)
-      err "Input length should be 1 mod 5" if string.bytesize % 5 != 1
+      if string.bytesize % 5 != 1
+        err "Input length should be 1 mod 5"
+      end
 
       counter = extract_counter(string)
       decoded = _decode(string)
