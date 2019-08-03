b932e11f5bca28c0b105d611f93e79d99bd2e38f
Xavier Noria <fxn@hashref.com>
2019-08-03 11:07:05 +0200

    use xmalloc() instead of malloc()
    With this function, the Ruby runtime is aware of the allocation, the
    garbage collector can free memory if needed, and if not possible you'll
    get a proper Ruby exception.
    
    Thanks to @vmg :heart:.

--- ext/z85/z85.c
+++ ext/z85/z85.c
@@ -60,28 +60,13 @@ static byte decoder[96] = {
     0x21, 0x22, 0x23, 0x4F, 0x00, 0x50, 0x00, 0x00
 };
 
-static void* z85_malloc(size_t size)
-{
-    void* ptr = malloc(size);
-
-    /*
-      malloc(0) is implementation-defined and could return NULL, so if ptr is
-      NULL, we need to check size too before raising. Note that free(NULL) is
-      valid (it does nothing).
-    */
-    if (!ptr && size)
-      rb_raise(rb_eNoMemError, "Out of memory");
-
-    return ptr;
-}
-
 static VALUE z85_encode(VALUE _mod, VALUE string)
 {
     byte* data = (byte*) StringValuePtr(string);
     long size = RSTRING_LEN(string);
 
     size_t encoded_len = size * 5 / 4;
-    char* encoded = z85_malloc(encoded_len + 1);
+    char* encoded = xmalloc(encoded_len + 1);
     uint char_nbr = 0;
     uint byte_nbr = 0;
     uint32_t value = 0;
@@ -100,7 +85,7 @@ static VALUE z85_encode(VALUE _mod, VALUE string)
     encoded[char_nbr] = 0;
 
     VALUE out = rb_usascii_str_new_cstr(encoded);
-    free(encoded);
+    xfree(encoded);
     return out;
 }
 
@@ -112,7 +97,7 @@ static VALUE z85_decode(VALUE _mod, VALUE rstring)
       strlen--; /* It is padded, ignore the counter */
 
     size_t decoded_size = strlen * 4 / 5;
-    byte* decoded = z85_malloc(decoded_size);
+    byte* decoded = xmalloc(decoded_size);
 
     uint byte_nbr = 0;
     uint char_nbr = 0;
@@ -130,7 +115,7 @@ static VALUE z85_decode(VALUE _mod, VALUE rstring)
     }
 
     VALUE out = rb_str_new((const char*) decoded, decoded_size);
-    free(decoded);
+    xfree(decoded);
     return out;
 }
 
